d0f3bbea9f6907949a3da506c278ccf33035ba7d
jebb king <gabouchard2002@gmail.com>
2024-03-23 20:42:31 +0300

    Adjusting settings

--- settings.py
+++ settings.py
@@ -9,19 +9,19 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Параметры корабля.
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
         self.ship_limit = 3
 
         # Параметры снаряда.
-        self.bullet_speed = 2.5
+        self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 4
 
         # Настройки пришельцев.
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
 
         # Настройка 1 означает движение слева направо, а настройка -1 означает противоположное. 
         self.fleet_direction = 1
